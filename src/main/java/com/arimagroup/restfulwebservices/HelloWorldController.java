2cdaae5880ab43474203e5736db26dcdf67c510b
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-09-10 16:26:46 -0500

    Hello world RestController - RequestMapping method=GET

---
+++ src/main/java/com/arimagroup/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.arimagroup.restfulwebservices;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller
+@RestController
+public class HelloWorldController {
+    // GET
+    // URI - /hello-world
+    // method - "Hello World"
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
